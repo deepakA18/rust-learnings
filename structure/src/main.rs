2036514e1648ec7a804007ca8414bf5027e8a4f6
deepakagashe18 <agashedeepak55@gmail.com>
2025-01-09 13:25:55 +0530

    learnt structure

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -5,11 +5,33 @@
 //     active: bool
 // }
 
+
 #[derive(Debug)]
 struct Rectangle{
     width: u32,
     height: u32
 }
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other:  &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle{
+        Rectangle{
+            width: size,
+            height: size
+        }
+    }
+}
+
+
 fn main() {
 //    let mut user1 = User{
 //     email: String::from("abc@gmail.com"),
@@ -39,8 +61,23 @@ let rect:Rectangle = Rectangle{
     height: 50,
 };
 
+let rect1: Rectangle = Rectangle{
+    width: 20, 
+    height: 40,
+};
+
+let rect2: Rectangle = Rectangle{
+    width: 40, 
+    height: 50,
+};
+
+let rect3:Rectangle = Rectangle::square(25);
+
+println!("The area of rect1 is: {}", rect.can_hold(&rect1));
+println!("The area of rect2 is: {}", rect.can_hold(&rect2));
+println!("The area of rect3 is: {:?}", rect3);
 println!("rect: {:?}", rect);
-println!("The area of rectangle is: {} ", area(&rect));
+println!("The area of rectangle is: {} ", rect.area());
 }
 
 // fn build_user(email: String, username: String) -> User{
@@ -54,6 +91,6 @@ println!("The area of rectangle is: {} ", area(&rect));
 // }
 
 
-fn area(rectangle: &Rectangle) -> u32{
-    rectangle.width * rectangle.height
-}
+// fn area(rectangle: &Rectangle) -> u32{
+//     rectangle.width * rectangle.height
+// }
